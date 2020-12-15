80487cccfb6f86cfecd7a76635ade60a3639da8c
Tarek Behairy <tbtarek@gmail.com>
2020-12-14 21:56:55 -0500

    Added variables for Characters,symbols and numbers, as wellas password criteria

--- script.js
+++ script.js
@@ -3,12 +3,31 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+// Varuables for all the characters, symbols and numbers
+  var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var numArray = ["0","1","2","3","4","5","6","7","8","9"]
+  var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+  
+  
+// password criteria
+  var charLength = prompt ("Choose length of password between 8 and 128");
+  var numbers = confirm ("Do you want numbers in your password?");
+  var uppercase = confirm ("Do you want Uppercase letters in your password?");
+  var lowercase = confirm ("Do you want lowercase letters in your password?");
+  var characters = confirm ("Do you want special characters in your password?");
+  
+  //conditions of Array
+  
+}
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
